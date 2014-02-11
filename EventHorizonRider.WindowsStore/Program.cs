4eeec169133dc8b91f1a8329cf21a01fbe19c8f3
Scott Lerch <scottlerch@gmail.com>
2014-02-10 21:40:04 -0800

    Add Windows Store support

---
+++ EventHorizonRider.WindowsStore/Program.cs
@@ -0,0 +1,16 @@
+﻿using EventHorizonRider.Core;
+
+namespace EventHorizonRider.WindowsStore
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<MainGame>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
