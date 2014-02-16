4d3c6536e994c444affa58d17ae29c935a6cc24a
Scott Lerch <scottlerch@gmail.com>
2014-02-15 22:52:52 -0800

    Add XNA content project and update font of score

---
+++ EventHorizonRider.WindowsXna/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace EventHorizonRider.WindowsXna
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // NOTE: this is a dummy project to force the XNA content project to be built
+        }
+    }
+#endif
+}
+
